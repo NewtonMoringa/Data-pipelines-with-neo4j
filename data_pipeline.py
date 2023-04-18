a559fcae5c79e9f117c493c57c453db1f43165e3
Newton <nkipngeno@safaricom.co.ke>
2023-04-18 19:40:32 +0300

    Update pipeline.

--- data_pipeline.py
+++ data_pipeline.py
@@ -4,9 +4,9 @@ import pandas as pd
 import psycopg2
 
 # Define Neo4j connection details
-neo4j_uri = "bolt://localhost:7687"
+neo4j_uri = "neo4j+s://f7fda941.databases.neo4j.io"
 neo4j_user = "neo4j"
-neo4j_password = "password"
+neo4j_password = "hsYhN8uXenrCRxxcyvT8S-MQ0dOKd5r76NNIfRtlBlk"
 
 # Define Postgres connection details
 pg_host = "localhost"
@@ -15,12 +15,20 @@ pg_user = "postgres"
 pg_password = "password"
 
 # Define Neo4j query to extract data
-neo4j_query = 
+neo4j_query = 'MATCH (n) RETURN n'
 
 
 # Define function to extract data from Neo4j and return a Pandas DataFrame
 def extract_data():
     # Connect to Neo4j
+    driver = GraphDatabase.driver(neo4j_uri, auth=(neo4j_user, neo4j_password))
+        with driver.session() as session:
+        # Execute Cypher query and retrieve results
+        result = session.run(neo4j_query)
+        # Convert results to pandas DataFrame
+        df = pd.DataFrame(result.records, columns=result.keys())
+    driver.close()
+    return df
 
 
 # Define function to transform data
@@ -29,6 +37,7 @@ def transform_data(df):
     df["start_date"] = pd.to_datetime(df["start_date"])
     
     # Remove null values
+    df = df.dropna()
      
     return df
 
@@ -48,19 +57,28 @@ def load_data(df):
             price FLOAT
         )
         """)
-    # Insert data into table
-    
+    #Insert data
+    try:
+        for index, row in df.iterrows():
+            cur.execute("""INSERT INTO telecom_data (customer_id, subscription_id, service_id, 
+            start_date, end_date, price) 
+            VALUES(%s, %s, %s, %s, %s, %s)
+            """, row['customer_id'], row['subscription_id'], row['service_id'], row['start_date'], row['end_date'], row['price'])
+    except:
+        return "Error inserting data"
     conn.commit()
     conn.close()
 
 # Define main function
 def main():
     # Extract data from Neo4j
+    data = extract_data()
     
     # Transform data using Pandas
+    transformed_data = transform_data(data)
     
     # Load data into Postgres
-    
+    load_data(transformed_data)
 
 # Call main function
 if __name__ == "__main__":
